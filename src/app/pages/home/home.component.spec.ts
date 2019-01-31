dc7fc50c0cbb013f9469fd8870a73181e0a58870
Lukas <lukas.dirda@gmail.com>
2019-01-31 20:42:24 +0200

    cleanup

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
@@ -8,6 +9,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule ],
       declarations: [ HomeComponent ]
     })
     .compileComponents();
