57aea7ae22930b52961ab4400bee53a95915b180
Lukas <lukas.dirda@gmail.com>
2019-01-31 20:34:32 +0200

    form example added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomeComponent } from './pages/home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+interface ICommentModel {
+  firstName: string;
+  lastName: string;
+  comment: string;
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  commentModel: ICommentModel = {
+    comment: '',
+    firstName: '',
+    lastName: ''
+  };
 
-  constructor() { }
+  inputMinLength = 3;
+  textAreaMinLength = 10;
+  inputMaxLength = 20;
+  textAreaMaxLength = 60;
 
-  ngOnInit() {
+  isSubmitted: boolean;
+
+  submitForm(): void {
+    console.log(this.commentModel);
+    this.isSubmitted = true;
   }
-
 }
